c9e3921677334dc9dc69169a53df3925debe747d
SardorproDev <sardorqobilov555@gmail.com>
2023-03-28 09:35:08 +0500

    header menu bottom section  finishd

--- js/script.js
+++ js/script.js
@@ -13,11 +13,31 @@ window.addEventListener('DOMContentLoaded', () => {
 	
 	try {
 		var swiper = new Swiper(".mySwiper", {
-			slidesPerView: 5,
-			spaceBetween: 16,
 			pagination: {
 				el: ".swiper-pagination",
 				clickable: true,
+			},
+			breakpoints: {
+				360: {
+					slidesPerView: 2,
+					spaceBetween: 8,
+				},
+				576: {
+					slidesPerView: 3,
+					spaceBetween: 12,
+				},
+				768: {
+					slidesPerView: 3,
+					spaceBetween: 16,
+				},
+				992: {
+					slidesPerView: 4,
+					spaceBetween: 20,
+				},
+				1920: {
+					slidesPerView: 5,
+					spaceBetween: 16,
+				},
 			},
 		});
 	} catch (error) {

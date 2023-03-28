04d2c13ede86500dc1e3ad45e41f614cbfbbb635
SardorproDev <sardorqobilov555@gmail.com>
2023-03-28 21:05:13 +0500

    orders html finshd

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ window.addEventListener('DOMContentLoaded', () => {
 	// header language
 	try {
 		const headerLaguabeBtn = document.querySelector('.default-lagnuage'),
-			showLanguageBox = document.querySelector('.header-language__box');
+			showLanguageBox = document.querySelector('.header-language__box'),
+			headerLanguageList = document.querySelectorAll('.header-language__box li'),
+			headerLanguageListImg = document.querySelectorAll('.header-language__box li img');
 
-		headerLaguabeBtn.addEventListener('click', () => {
-			showLanguageBox.classList.toggle('active');
-		})
+			headerLaguabeBtn.addEventListener('click', () => {
+				showLanguageBox.classList.toggle('active');
+			})
 	} catch (error) {
 		console.log(error);
 	}
@@ -43,6 +45,8 @@ window.addEventListener('DOMContentLoaded', () => {
 	} catch (error) {
 		console.log(error);
 	}
+
+	// ! slider
 	try {
 		var swiper = new Swiper(".mySwiper2", {
 			pagination: {
@@ -79,7 +83,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	} catch (error) {
 		console.log(error);
 	}
-	// 
+	// ! slect box
 	try {
 		const selectBox = document.querySelector('.select-box'),
 			settingBox = document.querySelector('.setting-box'),
@@ -95,6 +99,59 @@ window.addEventListener('DOMContentLoaded', () => {
 			settingBox.classList.toggle('setting-box__active');
 			settingBoxItems.classList.toggle('active');
 
+		})
+	} catch (error) {
+		console.log(error);
+	}
+	// ! order list
+	try {
+		const orderListHeaderBtn = document.querySelectorAll('.order-list__header'),
+			ordersProductBox = document.querySelectorAll('.orders-product__box');
+
+		orderListHeaderBtn.forEach((item, index) => {
+			item.addEventListener('click', (e) => {
+				ordersProductBox.forEach((a, i) => {
+					if (i != index) {
+						a.classList.remove('active')
+					}
+				})
+				orderListHeaderBtn.forEach((z, t) => {
+					if (t != index) {
+						z.classList.remove('active-icon')
+					}
+				})
+				ordersProductBox[index].classList.toggle('active');
+				e.target.classList.toggle('active-icon')
+			})
+		})
+	} catch (error) {
+		console.log(error);
+	}
+
+	try {
+		const ordersDeliveryBtn = document.querySelectorAll(".btn-green"),
+			gettingReadyBox = document.querySelectorAll('.getting-ready__box');
+
+		ordersDeliveryBtn.forEach((item, idx) => {
+			item.addEventListener('click', (e) => {
+				gettingReadyBox.forEach((x, y) => {
+					if (y != idx) {
+						x.classList.remove('active');
+					}
+				})
+				gettingReadyBox[idx].classList.toggle('active');
+				ordersDeliveryBtn.forEach((a, b) => {
+					if (b != idx) {
+						a.classList.remove('active-icon');
+						a.classList.remove('btn-green__active')
+					}
+				})
+				e.target.classList.toggle('active-icon');
+				e.target.classList.toggle('btn-green__active');
+				// if(e.target != idx) {
+				// 	e.target.classList.remove('active-icon');
+				// }
+			})
 		})
 	} catch (error) {
 		console.log(error);
